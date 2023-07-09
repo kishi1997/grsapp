0dcb3cd81122a9636b6877b62a8ebe20f9ffb116
tomo <tomo9634@gmail.com>
2023-07-09 15:48:11 +1200

    ボタンを押した際にレポジトリ詳細を表示（ボタンの文言も変更）をリポジトリそれぞれで動くように変更

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -4,23 +4,24 @@ import { useState } from 'react'
 import axios from 'axios'
 import classes from '../styles/components/Main.module.scss'
 
-  interface Repo {
-    id: number;
-    name: string;
-    html_url: string;
-    owner: {
-      avatar_url: string;
-    };
-    language: string;
-    stargazers_count: number;
-    watchers_count: number;
-    forks_count: number;
-    open_issues_count: number;
-  }
+interface Repo {
+  id: number;
+  name: string;
+  html_url: string;
+  owner: {
+    avatar_url: string;
+  };
+  language: string;
+  stargazers_count: number;
+  watchers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+}
 
 const Main = () => {
   const [keyword, setKeyword] = useState('');
   const [repos, setRepos] = useState<Repo[]>([]);
+  const [showDetails, setShowDetails] = useState(false);
 
   const searchRepos = async () => {
     try {
@@ -30,6 +31,18 @@ const Main = () => {
       setRepos(response.data.items);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const [openIds, setOpenIds] = useState(null);
+
+  const toggleDetails = (id:number) => {
+    if (openIds === id) {
+      setOpenIds(null);
+      console.log(openIds, 'nullだよ');
+    } else {
+      setOpenIds(id);
+      console.log(openIds);
     }
   };
 
@@ -44,20 +57,33 @@ const Main = () => {
           onChange={(e) => setKeyword(e.target.value)}
         />
       </div>
-      <button onClick={searchRepos}>Search</button>
+      <button className={classes.primaryBtn} onClick={searchRepos}>Search</button>
       <ul>
-        {repos.map((repo: any) => (
-          <li key={repo.id}>
-            <a href={repo.html_url} target='_blank' rel="noopener noreferrer">
-            <img src={repo.owner.avatar_url} alt="Owner Avatar" />
-            <div>{repo.name}</div>
-            <div>Language: {repo.html_url}</div>
-            <div>Language: {repo.language}</div>
-            <div>Stars: {repo.stargazers_count}</div>
-            <div>Watchers: {repo.watchers_count}</div>
-            <div>Forks: {repo.forks_count}</div>
-            <div>Issues: {repo.open_issues_count}</div>
-            </a>
+        {repos.map((repo) => (
+          <li key={repo.id} className={classes.repo}>
+            <div className={classes.repo__title}>
+              <h2>{repo.name} {repo.id}</h2>
+              <div className={classes.repo__btn1}>
+                <button onClick={() => toggleDetails(repo.id)}>
+                  {openIds === repo.id ? '閉じる' : '詳細を見る'}
+                </button>
+              </div>
+            </div>
+            {openIds === repo.id &&
+                <div>
+                  <div className={classes.repo__detail}>
+                    <div className={classes.repo__icon}><img src={repo.owner.avatar_url} alt="Owner Avatar" /></div>
+                    <div>プロジェクト言語: {repo.language}</div>
+                    <div>Star数: {repo.stargazers_count}</div>
+                    <div>Watcher数: {repo.watchers_count}</div>
+                    <div>Fork数: {repo.forks_count}</div>
+                    <div>Issue数: {repo.open_issues_count}</div>
+                  </div>
+                  <div className={classes.primaryBtn}>
+                    <a href={repo.html_url} target='_blank' rel="noopener noreferrer">このリポジトリを見る</a>
+                  </div>
+                </div>
+            }
           </li>
         ))}
       </ul>
