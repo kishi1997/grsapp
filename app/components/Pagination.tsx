d22bcac5ef48075d9715c45976ebc03e33740ca3
tomo <tomo9634@gmail.com>
2023-07-18 18:10:08 +1200

    paginationのcssがモジュールだと当たらないため、cssファイルを追加

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,40 +1,45 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactPaginate from 'react-paginate';
 import classes from '../styles/components/Pagination.module.scss';
-import { searchResultState } from '../states/atoms/searchResultState';
+import '../styles/pagination-parts.css';
+// recoil
 import { useRecoilState, useRecoilValue } from 'recoil';
+import { searchResultState } from '../states/atoms/searchResultState';
 import { itemsOffsetState } from '../states/atoms/itemsOffsetState';
-import { currentReposSelector } from '../states/selector/currentReposSelector';
 
 const Pagination = () => {
-    // 検索結果のリポジトリ
+
+    // -------------検索結果のリポジトリ
     const repos = useRecoilValue(searchResultState);
-    // 現在表示されているリポジトリ５つ
-    const currentRepos = useRecoilValue(currentReposSelector);
-    // 1ページに表示するリポジトリ数
+
+    // -------------1ページに表示するリポジトリ数
     const itemsPerPage = 5;
-    // ページの先頭に表示するリポジトリの状態管理
+
+    // -------------ページの先頭に表示するリポジトリの状態管理
     const [itemsOffset, setItemsOffset] = useRecoilState(itemsOffsetState);
-    // 現在のページ数の計算
+
+    // -------------現在のページ数の計算
     const pageCount = Math.ceil(repos.length / itemsPerPage);
-    // ページの先頭に表示するリポジトリが何番目のリポジトリかを計算
+
+    // -------------ページの先頭に表示するリポジトリが何番目のリポジトリかを計算
     const handlePageChange = (e: { selected: number }) => {
         const newOffset = (e.selected * itemsPerPage) % repos.length;
         setItemsOffset(newOffset);
     }
 
     return (
+
         <>
-            {/* <Repositories repos={repos} currentRepos={currentRepos} /> */}
             {repos.length > 0 &&
-                <div className={classes.container}>
+                <div className={classes.pagination}>
                     <ReactPaginate
                         pageCount={pageCount}
                         onPageChange={handlePageChange}
                         breakLabel="..."
-                        nextLabel="next >"
-                        pageRangeDisplayed={5}
-                        previousLabel="< previous"
+                        nextLabel="次へ >"
+                        pageRangeDisplayed={2}
+                        marginPagesDisplayed={2}
+                        previousLabel="< 前へ"
                         renderOnZeroPageCount={null}
                     />
                 </div>
