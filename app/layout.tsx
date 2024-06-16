16da5b4158a37701cb10c55f59592e9300259165
tomo <tomo9634@gmail.com>
2024-06-16 21:06:17 +0900

    metadataを直接書き込むように修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,30 @@
-'use client'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { NextAuthProvider } from "./providers/NextAuth"
-import { RecoilRoot } from 'recoil'
+"use client";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { NextAuthProvider } from "./providers/NextAuth";
+import { RecoilRoot } from "recoil";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Serch Github repositories',
-  description: 'Serch Github repositories by keyword quickly',
-}
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <title>Serch Github repositories</title>
+        <meta
+          name="description"
+          content="Serch Github repositories by keyword quickly"
+        />
         <RecoilRoot>
-        <NextAuthProvider>
-          <div>
-            {children}
-          </div>
-        </NextAuthProvider>
+          <NextAuthProvider>
+            <div>{children}</div>
+          </NextAuthProvider>
         </RecoilRoot>
       </body>
     </html>
-  )
+  );
 }
